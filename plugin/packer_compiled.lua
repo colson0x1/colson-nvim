b7cd24460a349bb63179ae9cd1761c4a956e27de
Colson <colsoncalifornication@gmail.com>
2023-10-16 00:16:14 +0545

    configure undotree

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -109,6 +109,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/colson/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
+  undotree = {
+    loaded = true,
+    path = "/Users/colson/.local/share/nvim/site/pack/packer/start/undotree",
+    url = "https://github.com/mbbill/undotree"
   }
 }
 
