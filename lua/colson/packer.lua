fd42b2da30a2b4cfb1438e79824a3d36e78331b5
Colson <colsoncalifornication@gmail.com>
2023-10-15 22:47:47 +0545

    configure color scheme

--- lua/colson/packer.lua
+++ lua/colson/packer.lua
@@ -7,9 +7,17 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  use { 
-    'nvim-telescope/telescope.nvim', tag = '0.1.4',
-    -- or                            , branch = '0.1.x',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+	use { 
+		'nvim-telescope/telescope.nvim', tag = '0.1.4',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use({ 
+		'rose-pine/neovim', 
+		as = 'rose-pine',
+		config = function()
+			vim.cmd('colorscheme rose-pine')
+		end
+	})
 end)
