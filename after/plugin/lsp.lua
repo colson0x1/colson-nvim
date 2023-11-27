6b4961e758e8d82ef33f21e0ddccc6ae92182d30
Colson <colsoncalifornication@gmail.com>
2023-11-27 10:00:34 +0545

    remap selection in autocompletion

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -84,6 +84,7 @@ cmp.setup({
 		["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
 		["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
 		["<C-y>"] = cmp.mapping.confirm({ select = true }),
+		["<C-k>"] = cmp.mapping.confirm({ select = true }),
 		["<C-Space>"] = cmp.mapping.complete(),
 	}),
 })
