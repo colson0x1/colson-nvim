2f075d0fbdf3a668f5a69e99e55c0046810b587b
Colson <colsoncalifornication@gmail.com>
2024-02-15 12:54:52 +0545

    resolve debounce

--- after/plugin/neocord.lua
+++ after/plugin/neocord.lua
@@ -10,7 +10,7 @@ neocord.setup({
 	main_image = "language", -- "language" or "logo"
 	client_id = "1157438221865717891", -- Use your own Discord application client id (not recommended)
 	log_level = nil, -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
-	debounce_timeout = 10, -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
+	debounce_timeout = 86400, -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
 	blacklist = {}, -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
 	file_assets = {}, -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
 	show_time = true, -- Show the timer
