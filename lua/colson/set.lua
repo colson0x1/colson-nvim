4ddcd9071a2f5a45d20a6a53bfb378510cb6618e
Colson <colsoncalifornication@gmail.com>
2024-02-16 14:09:43 +0545

    add bufferline leader prefix

--- lua/colson/set.lua
+++ lua/colson/set.lua
@@ -32,3 +32,17 @@ vim.opt.colorcolumn = "80"
 
 -- JSX/TSX comment
 vim.g.skip_ts_context_commentstring_module = true
+
+-- /* Bufferline Tabs */
+-- Switching buffers
+vim.api.nvim_set_keymap("n", "<leader>b", ":bprev<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>B", ":bnext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>bd", ":bdelete<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>bf", ":bfirst<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>bl", ":blast<CR>", { noremap = true, silent = true })
+
+-- Closing tabs
+vim.api.nvim_set_keymap("n", "<leader>td", ":tabclose<CR>", { noremap = true, silent = true })
+
+-- Closing current buffer
+vim.api.nvim_set_keymap("n", "<leader>bd", ":bdelete<CR>", { noremap = true, silent = true })
